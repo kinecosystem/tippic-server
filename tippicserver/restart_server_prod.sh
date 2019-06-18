c2de532dfc2f3610e9e1ef618f847e6cd39a7c91
Berry Ventura Lev <berry@kik.com>
2019-06-18 14:36:27 +0300

    fix restart script

--- tippicserver/restart_server_prod.sh
+++ tippicserver/restart_server_prod.sh
@@ -1,4 +1,4 @@
 export AWS_REGION='us-east-1'
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # https://github.com/ansible/ansible/issues/32499
 
-ansible-playbook playbooks/tippic-server-prod-restart-only.yml -i tippic-server-prod-1,tippic-server-prod-2 --extra-vars "tippic_prod_tg_arn='arn:aws:elasticloadbalancing:us-east-1:935522987944:targetgroup/tippic-prod/44e68fe465dd36c9'"
+ansible-playbook playbooks/tippic-server-prod-restart-only.yml -i tippic-server-prod-3, --extra-vars "tippic_prod_tg_arn='arn:aws:elasticloadbalancing:us-east-1:935522987944:targetgroup/tippic-prod-3/85f60c33add4fbce'"
